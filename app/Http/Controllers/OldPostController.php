37853b1b4a9529aa71bdc69357d823c756c4da3f
ShuntaroKishida <137754393+ShuntaroKishida@users.noreply.github.com>
2023-10-08 13:18:54 +0900

    10-1終了

--- app/Http/Controllers/OldPostController.php
+++ app/Http/Controllers/OldPostController.php
@@ -2,31 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Post;
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        $posts = Post::all();
-        return view('post.index', compact('posts'));
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         return view('post.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         $validated = $request->validate([
@@ -35,28 +20,22 @@ class PostController extends Controller
         ]);
         $validated['user_id'] = auth()->id();
         $post = Post::create($validated);
-        return redirect()->route('post.index')->with('message', '保存しました');
+        return back()->with('message', '保存しました');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Post $post)
-    {
+    public function index(){
+        $posts = Post::with('user')->get();
+        return view('post.index', compact('posts'));
+    }
+
+    public function show(Post $post){
         return view('post.show', compact('post'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Post $post)
-    {
+    public function edit(Post $post){
         return view('post.edit', compact('post'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Post $post)
     {
         $validated = $request->validate([
@@ -65,14 +44,10 @@ class PostController extends Controller
         ]);
         $validated['user_id'] = auth()->id();
         $post->update($validated);
-        return redirect()->route('post.show', compact('post'))->with('message', '更新しました');
+        return back()->with('message', '更新しました');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Post $post)
-    {
+    public function destroy(Post $post){
         $post->delete();
         return redirect()->route('post.index')->with('message', '削除しました');
     }
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,9 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TestController;
 use App\Http\Controllers\PostController;
+
+Route::resource('post', PostController::class);
 
 /*
 |--------------------------------------------------------------------------
@@ -22,37 +23,12 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-Route::get('/test', [TestController::class, 'test'])
-->name('test');
-
-Route::get('post/create', [PostController::class, 'create'])
-->middleware(['auth', 'admin']);
-
-Route::post('post', [PostController::class, 'store'])
-->name('post.store');
-
-Route::get('post', [PostController::class, 'index'])
-->middleware(['auth'])
-->name('post.index');
-
-Route::get('post/show/{post}', [PostController::class, 'show'])
-->name('post.show');
-
-Route::get('post/{post}/edit', [PostController::class, 'edit'])
-->name('post.edit');
-
-Route::patch('post/{post}', [PostController::class, 'update'])
-->name('post.update');
-
-Route::delete('post/{post}', [PostController::class, 'destroy'])
-->name('post.destroy');
 
 require __DIR__.'/auth.php';
